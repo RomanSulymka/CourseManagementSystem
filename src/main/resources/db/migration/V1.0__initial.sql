ca9d699f7bad50370917af4624e66d0509edfec0
Roman Sulymka <roma.sulimak@gmail.com>
2023-11-26 23:42:36 +0200

    added constraint

--- src/main/resources/db/migration/V1.0__initial.sql
+++ src/main/resources/db/migration/V1.0__initial.sql
@@ -76,7 +76,8 @@ CREATE TABLE user_course_marks
     total_score NUMERIC(10, 2),
     passed      BOOLEAN NOT NULL DEFAULT false,
     FOREIGN KEY (user_id) REFERENCES users (id),
-    FOREIGN KEY (course_id) REFERENCES courses (id)
+    FOREIGN KEY (course_id) REFERENCES courses (id),
+    CONSTRAINT unique_mark_user_course UNIQUE (user_id, course_id)
 );
 
 CREATE TABLE tokens
