070145d1e083cc88acbd6513c18fa01beae41d7d
Roman Sulymka <roma.sulimak@gmail.com>
2023-12-10 15:03:50 +0200

    fixed e2e tests, changed response entity

--- src/test/java/edu/sombra/coursemanagementsystem/e2e/coursefeedback/AdminCourseFeedbackControllerE2ETest.java
+++ src/test/java/edu/sombra/coursemanagementsystem/e2e/coursefeedback/AdminCourseFeedbackControllerE2ETest.java
@@ -163,11 +163,11 @@ class AdminCourseFeedbackControllerE2ETest {
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(jwtToken);
 
-        ResponseEntity<GetCourseFeedbackDTO> responseEntity = restTemplate.exchange(
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
                 buildUrl("/api/v1/feedback/{id}", feedbackId),
                 HttpMethod.GET,
                 new HttpEntity<>(headers),
-                GetCourseFeedbackDTO.class
+                String.class
         );
 
         assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
