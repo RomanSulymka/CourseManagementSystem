e66c60ec54a4f32dd95ab7db5e76edd0b0c40885
Roman Sulymka <roma.sulimak@gmail.com>
2023-12-18 19:43:52 +0200

    [BE] user can register only as a student

--- src/test/java/edu/sombra/coursemanagementsystem/controller/auth/AuthenticationControllerTest.java
+++ src/test/java/edu/sombra/coursemanagementsystem/controller/auth/AuthenticationControllerTest.java
@@ -3,7 +3,6 @@ package edu.sombra.coursemanagementsystem.controller.auth;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.sombra.coursemanagementsystem.dto.auth.AuthenticationDTO;
 import edu.sombra.coursemanagementsystem.dto.auth.RegisterDTO;
-import edu.sombra.coursemanagementsystem.enums.RoleEnum;
 import edu.sombra.coursemanagementsystem.service.auth.AuthenticateService;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -47,7 +46,7 @@ class AuthenticationControllerTest {
 
     @Test
     void registerUser_ValidRequest_ReturnsOk() throws Exception {
-        RegisterDTO registerDTO = new RegisterDTO("firstName", "lastName", "email", "password", RoleEnum.STUDENT);
+        RegisterDTO registerDTO = new RegisterDTO("firstName", "lastName", "email", "password");
 
         mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/auth/register")
                         .contentType(MediaType.APPLICATION_JSON)
