a089fa60e7991b1779a6bc0993c9c3910971f6c2
Roman Sulymka <roma.sulimak@gmail.com>
2023-12-21 23:20:19 +0200

    [BE] Removed unused constructor

--- src/main/java/edu/sombra/coursemanagementsystem/exception/CourseCreationException.java
+++ src/main/java/edu/sombra/coursemanagementsystem/exception/CourseCreationException.java
@@ -2,10 +2,6 @@ package edu.sombra.coursemanagementsystem.exception;
 
 public class CourseCreationException extends RuntimeException {
 
-    public CourseCreationException(String message) {
-        super(message);
-    }
-
     public CourseCreationException(Exception message) {
         super(message);
     }
